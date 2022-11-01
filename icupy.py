3fe30137ad358b933024f8d84e0cc325b348f26b
marlengabrich <marlen.gabrich@mi.unc.edu.ar>
2022-11-01 13:50:39 -0300

    statical criteria & gropby ok

--- icupy.py
+++ icupy.py
@@ -70,14 +70,3 @@ class Standary():
         
         return daset
 
-    def standa01(self,setdatos):
-
-        arraset = setdatos.to_xarray()
-        arraset['datetime'].isoformat()
-        period = arraset['datetime'].to_period("Y")
-        groupe = pd.Grouper(level=0,freq='H')
-        arraset.groupby([period,groupe]).count()
-
-        return arraset
-
-
